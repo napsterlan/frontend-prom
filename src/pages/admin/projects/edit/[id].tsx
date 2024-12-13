658abc52dd4f9f723bf403564807274568e3a361
Valery Shatilov <napster.lan@gmail.com>
2024-12-13 09:49:24 +0300

    Множественные изменения

--- src/pages/admin/projects/edit/[id].tsx
+++ src/pages/admin/projects/edit/[id].tsx
@@ -1,96 +1,66 @@
-import { useState } from 'react';
-import { updateProjectById, uploadFiles } from '@/api/apiClient';
-import { getAllProjectCategories, getProjectById } from '@/api/apiClient';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { ProjectCategory } from '@/types/types';
-import { Project } from '@/types/types';
+import { getProjectById, updateProjectById } from '../../../../api/apiClient';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+// import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
-// Функция для получения данных на сервере
-export const getServerSideProps = async (context: { params: { id: number } }) => {
-  const { id } = context.params;
-  let project = {};
-  let categories = [];
-  
-  try {
-    const categoriesResponse = await getAllProjectCategories();
-    categories = categoriesResponse.data;
-
-    const response = await getProjectById(Number(id));
-    project = response.data; // Получаем данные проекта
-    console.log(project);
-  } catch (error) {
-    console.error('Ошибка загрузки проекта:', error);
-  }
-
-  return {
-    props: {
-      project, // Передаем данные проекта в компонент
-      categories, // Передаем данные категорий в компонент
-    },
-  };
-};
-
-const EditProject = ({ project, categories }: { project: Project, categories: ProjectCategory[] }) => {
+const EditProject = () => {
   const router = useRouter();
+  const { id } = router.query;
   const [formData, setFormData] = useState<{
     title: string;
     description: string;
     metaTitle: string;
     metaDescription: string;
     metaKeyword: string;
-    ProjectCategories: any[];
     Slug: string;
-    relatedProducts: any[];
-    existingImages: any[];
-    newImages: File[];
-    deletedImageIds: number[];
+    relatedProducts: string;
+    // projectImages: {
+    //     ImageURL: string;
+    //     AltText: string;
+    // }[];
   }>({
-    title: project.Title || '',
-    description: project.Description || '',
-    metaTitle: project.MetaTitle || '',
-    metaDescription: project.MetaDescription || '',
-    metaKeyword: project.MetaKeyword || '',
-    ProjectCategories: project.ProjectCategories || [],
-    Slug: project.Slug || '',
-    relatedProducts: project.RelatedProducts || [],
-    existingImages: project.Images || [],
-    newImages: [],
-    deletedImageIds: [],
+    title: '',
+    description: '',
+    metaTitle: '',
+    metaDescription: '',
+    metaKeyword: '',
+    Slug: '',
+    relatedProducts: '',
+    // projectImages: [],
   });
-
-  const handleImageDelete = (index: number, isExisting: boolean) => {
-    if (isExisting) {
-      const imageToDelete = formData.existingImages[index];
-      setFormData({
-        ...formData,
-        existingImages: formData.existingImages.filter((_, i) => i !== index),
-        deletedImageIds: [...formData.deletedImageIds, imageToDelete.ID]
-      });
-    } else {
-      setFormData({
-        ...formData,
-        newImages: formData.newImages.filter((_, i) => i !== index)
-      });
+  useEffect(() => {
+    if (id !== undefined) {
+   
+      const fetchProject = async () => {
+        try {
+          const project = await getProjectById(Number(id));
+          console.log(project);
+          setFormData({
+            title: project.data.Title || '',
+            description: project.data.Description || '',
+            metaTitle: project.data.MetaTitle || '',
+            metaDescription: project.data.MetaDescription || '',
+            metaKeyword: project.data.MetaKeyword || '',
+            Slug: project.data.Slug || '',
+            relatedProducts: project.data.RelatedProducts || ''
+            // projectImages: project.data.ProjectImages.map((img: any) => ({
+            //     ImageURL: img.ImageURL,
+            //     AltText: img.AltText,
+            // })),
+          });
+        } catch (error) {
+          console.error('Ошибка загрузки проекта:', error);
+        }
+      };
+      fetchProject();
     }
-  };
+  }, [id]);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     console.log('Отправляемые данные:', formData);
     try {
-      // Загружаем только новые файлы
-      let uploadedImages = [];
-      if (formData.newImages.length > 0) {
-        const uploadResponse = await uploadFiles(
-          formData.newImages,
-          `/projects/${formData.title}`
-        );
-        uploadedImages = uploadResponse.filePaths.map((imageUrl: string) => ({
-          ImageURL: imageUrl,
-          AltText: '',
-        }));
-      }
-
       const projectData = {
         Title: formData.title,
         ProjectCategories: formData.ProjectCategories,
@@ -99,16 +69,6 @@ const EditProject = ({ project, categories }: { project: Project, categories: Pr
         MetaDescription: formData.metaDescription,
         MetaKeyword: formData.metaKeyword,
         Slug: formData.Slug || formData.title.replace(/\s+/g, '-').toLowerCase() || '',
-        RelatedProducts: formData.relatedProducts,
-        ProjectImages: [
-          ...formData.existingImages.map(img => ({
-            ID: img.ID,
-            ImageURL: img.ImageURL,
-            AltText: img.AltText
-          })),
-          ...uploadedImages
-        ],
-        deletedImageIds: formData.deletedImageIds
       };
 
       const response = await updateProjectById(Number(router.query.id), projectData);
@@ -142,42 +102,56 @@ const EditProject = ({ project, categories }: { project: Project, categories: Pr
             <label htmlFor="file-upload" className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md cursor-pointer">
               Добавить
             </label>
-            <div className="flex flex-wrap mt-2">
-              {/* Существующие изображения */}
-              {formData.existingImages.map((file: any, index: number) => (
-                <div key={`existing-${index}`} className="relative w-48 h-48 m-2">
-                  <img
-                    src={file.ImageURL}
-                    alt={file.AltText || `Изображение ${index + 1}`}
-                    className="w-full h-full object-cover rounded"
-                  />
-                  <button
-                    onClick={() => handleImageDelete(index, true)}
-                    className="absolute top-0 right-0 bg-red-500 text-white w-8 h-8"
+            <DragDropContext onDragEnd={(result: any) => {
+              const { source, destination } = result;
+              if (!destination) return;
+
+              const reorderedImages = Array.from(formData.projectImages);
+              const [removed] = reorderedImages.splice(source.index, 1);
+              reorderedImages.splice(destination.index, 0, removed);
+
+              setFormData({ ...formData, projectImages: reorderedImages });
+            }}>
+              <Droppable droppableId="droppable">
+                {(provided: any) => (
+                  <div
+                    className="flex flex-wrap mt-2"
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
                   >
-                    &times;
-                  </button>
-                </div>
-              ))}
-              
-              {/* Новые изображения */}
-              {formData.newImages.map((file: File, index: number) => (
-                <div key={`new-${index}`} className="relative w-48 h-48 m-2">
-                  <img
-                    src={URL.createObjectURL(file)}
-                    alt={`Новое изображение ${index + 1}`}
-                    className="w-full h-full object-cover rounded"
-                  />
-                  <button
-                    onClick={() => handleImageDelete(index, false)}
-                    className="absolute top-0 right-0 bg-red-500 text-white w-8 h-8"
-                  >
-                    &times;
-                  </button>
-                </div>
-              ))}
-            </div>
-          </div>
+                    {formData.projectImages.map((file, index) => (
+                      <Draggable key={index} draggableId={`draggable-${index}`} index={index}>
+                        {(provided: any) => (
+                          <div
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                            className="relative w-48 h-48 m-2"
+                          >
+                            <img
+                              src={URL.createObjectURL(file)}
+                              alt={`Изображение ${index + 1}`}
+                              className="w-full h-full object-cover rounded"
+                            />
+                            <button
+                              onClick={() => {
+                                const updatedImages = formData.projectImages.filter((_, i) => i !== index);
+                                setFormData({ ...formData, projectImages: updatedImages });
+                              }}
+                              className="absolute top-0 right-0 bg-red-500 text-white w-8 h-8"
+                            >
+                              &times;
+                            </button>
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            </DragDropContext>
+          </div> */}
         </div>
         <div className="w-1/2 p-4">
           <h1 className="text-2xl font-bold mb-6">Добавить новый проект</h1>
@@ -266,6 +240,27 @@ const EditProject = ({ project, categories }: { project: Project, categories: Pr
       </div>
     </div>
   );
+};
+
+// Функция для получения данных на сервере
+export const getServerSideProps = async (context: any) => {
+  const { id } = context.params;
+  let project = {};
+  console.log(id);
+
+  try {
+    const response = await getProjectById(Number(id));
+    project = response.data; // Получаем данные проекта
+    console.log(project);
+  } catch (error) {
+    console.error('Ошибка загрузки проекта:', error);
+  }
+
+  return {
+    props: {
+      project, // Передаем данные проекта в компонент
+    },
+  };
 };
 
 export default EditProject;
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -36,35 +36,20 @@ export default function ProjectsPage({ projects, categories }: { projects: Proje
         <h1 className="text-2xl font-bold">Проекты</h1>
       </div>
 
-      <div className="flex">
-        <aside className="w-1/4 p-4 border-r">
-          <h2 className="text-lg font-bold mb-4">Категории проектов</h2>
-          <ul>
-            {categories.map(category => (
-              <li key={category.ID} className="mb-2">
-                <Link href={`/projects/${category.Slug}`} className="text-blue-600 hover:underline">
-                  {category.Name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </aside>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-4 w-3/4">
-          {projects.map((project: Project) => (
-            project.ProjectsCategories.length > 0 ? (
-              <Link href={`/projects/${project.ProjectsCategories[0].Slug}/${project.Slug}`} key={project.ID} className="relative border rounded-lg overflow-hidden">
-                {project.Images?.[0] && (
-                  <div 
-                    style={{ backgroundImage: `url(${project.Images[0].ImageURL})` }} 
-                    className="w-full h-48 bg-cover bg-center rounded mb-4"
-                  />
-                )}
-                <h3 className="text-xl font-semibold mb-2">{project.Title}</h3>
-                <p className="text-gray-600">{project.Description}</p>
-              </Link>
-            ) : null
-          ))}
-        </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {projects.map(project => (
+          <div key={project.ProjectID} className="relative border rounded-lg p-4">
+            {project.ProjectImages?.[0] && (
+              <img 
+                src={project.ProjectImages[0].ImageURL} 
+                alt={project.ProjectImages[0].AltText} 
+                className="w-full h-48 object-cover rounded mb-4"
+              />
+            )}
+            <h3 className="text-xl font-semibold mb-2">{project.Title}</h3>
+            <p className="text-gray-600">{project.Description}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
